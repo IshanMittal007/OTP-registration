8118e13f188285fbafe3471a043ebb67b32ac73e
IshanMittal007 <mittalf1@gmail.com>
2026-09-23 15:58:13 +0530

    added refreshToken in userSchema

--- models/User.js
+++ models/User.js
@@ -4,6 +4,7 @@ const userSchema = new mongoose.Schema({
     name : { type: String, required: true },
     email : { type: String, required: true, unique: true,lowercase: true },
     password : { type: String, required: true },
+    refreshToken : { type: String, default: null},
     createdAt : { type: Date, default: Date.now },
 });
 
