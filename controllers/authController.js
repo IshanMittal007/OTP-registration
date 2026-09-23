791bb7e9e2308c438509c2d4f8057b4a7e1c5bde
IshanMittal007 <mittalf1@gmail.com>
2026-09-23 16:40:44 +0530

    added token generator for login and delete after logout

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,7 +1,25 @@
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 const User = require('../models/User');
 const Otp = require('../models/Otp');
 const sendEmail = require('../utils/sendEmail');
+
+// Token generation
+const generateAccessToken = (user) => {
+    return jwt.sign(
+        { userId: user._id, email: user.email },
+        process.env.ACCESS_TOKEN_SECRET,
+        { experiesIN: '15m'}
+    );
+};
+
+const generateRefreshToken = (user) => {
+    return jwt.sign(
+        { userId: user._id },
+        process.env.REFRESH_TOKEN_SECRET,
+        { expiresIn: '7d'}
+    );
+};
 
 // signUP
 exports.signup = async (req, res) => {
@@ -97,6 +115,19 @@ exports.login = async(req, res) => {
             return res.status(400).json({ message: 'Invalid password!' });
         }
 
+        const accessToken = generateAccessToken(user);
+        const refreshToken = generateAccessToken(user);
+
+        user.refreshToken = refreshToken;
+        await user.save();
+
+        res.cookie('refreshToken', refreshToken, {
+            httpOnly: true,
+            secure: process.env.NODE_ENV == 'production',
+            sameSite: 'strict',
+            maxAge: 7 * 24 * 60 * 60 * 1000
+        });
+
         res.status(200).json({
             message: 'Login successful.',
             user : { id: user._id, name: user.name, email: user.email }
@@ -104,4 +135,52 @@ exports.login = async(req, res) => {
     } catch (error) {
         res.status(500).json({ message: error.message });
     }
+};
+
+// Token refresh
+exports.refreshToken = async (req, res) => {
+    try {
+        const token = req.cookie.refreshToken;
+        if(!token) {
+            return res.status(401).json({ message: 'No refresh token provided.' });
+        }
+
+        const user = await User.findOne({ refreshToken: token });
+        if(!user) {
+            return res.status(403).json({ message: 'Session expired or invalidated. Plese log in again.' });
+        }
+
+        jwt.verify(token, process.env.REFRESH_TOKEN_SECRET, (err, decoded) => {
+            if (err) {
+                return res.status(403).json({ message: 'Invalid or expired refresh token.' });
+            }
+
+            const newAccessToken = generateAccessToken(user);
+            res.status(200).json({ accessToken: newAccessToken });
+        });
+    } catch (err) {
+        console.error('Refresh error:', err);
+        res.status(500).json({ message: 'Server error while refreshing token.' });
+    }
+};
+
+// logout - clear cookies
+exports.logout = async (req, res) => {
+  try {
+    const token = req.cookies.refreshToken;
+    if (token) {
+      await User.findOneAndUpdate({ refreshToken: token }, { refreshToken: null });
+    }
+
+    res.clearCookie('refreshToken', {
+      httpOnly: true,
+      sameSite: 'strict',
+      secure: process.env.NODE_ENV === 'production'
+    });
+
+    res.status(200).json({ message: 'Logged out successfully.' });
+  } catch (err) {
+    console.error('Logout error:', err);
+    res.status(500).json({ message: 'Server error during logout.' });
+  }
 };
