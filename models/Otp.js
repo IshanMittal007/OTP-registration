6ee3c9d7c1f7374162c525c8441d2ebc5fabb52d
IshanMittal007 <mittalf1@gmail.com>
2026-09-26 22:20:04 +0530

    changed some field requirement

--- models/Otp.js
+++ models/Otp.js
@@ -1,9 +1,9 @@
 const mongoose = require('mongoose');
 
 const otpSchema = new mongoose.Schema({
-    name : { type: String, required: true },
+    name : { type: String },
     email : { type: String, required: true, unique: true,lowercase: true },
-    password : { type: String, required: true },
+    password : { type: String },
     otp: { type: String, required: true },
     createdAt : { type: Date, default: Date.now, index: { expires: 300 } }
 });
